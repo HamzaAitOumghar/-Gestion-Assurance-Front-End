1c874e3d7e008e70f852726c9e3582665e67e487
EL Mansouri EL Houssin <houssin.mansouri93@gmail.com>
2017-12-18 14:33:10 +0000

    modifier sur interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MainModule } from './main/main.module';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MainModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/main/main.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { FooterComponent } from './footer/footer.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { ContentComponent } from './content/content.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MainComponent } from './main.component';
+
+
+@NgModule({
+    declarations: [
+    FooterComponent,
+    NavbarComponent,
+    ToolbarComponent,
+    ContentComponent,
+    MainComponent
+    ],
+    imports     : [
+        RouterModule
+    ],
+    exports: [MainComponent]
+})
+
+export class MainModule {
+}
